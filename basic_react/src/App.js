7b5684b59bb6ffd6631e2c7e4784d896bedaae6d
SHLpy <sohail786619@gmail.com>
2024-06-30 07:46:24 +0530

    rect prectivce

---
+++ basic_react/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Sohail_Codes</h1>
+  );
+}
+
+export default App;
---
+++ vite_react/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Sohail-Codes</h1>
+  )
+}
+
+export default App
